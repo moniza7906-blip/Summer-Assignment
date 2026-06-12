79f274c6cd9d29daf294ab6fb06ac670591b5da9
moniza7906-blip <moniza7906@gmail.com>
2026-06-12 17:54:14 +0530

    Day12 assignment

---
+++ Day12/q4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int perfect(int n)
+{
+    int sum=0,i;
+    for(i=1;i<n;i++)
+    {
+        if(n%i==0)
+        sum+=i;
+    }
+        if(sum==n)
+        return 1;
+        else
+        return 0;
+}
+int main()
+{
+    int num;
+    printf("enter a number");
+    scanf("%d",&num);
+    if(perfect(num))
+    printf("perfect number");
+    else
+    printf("not a perfect number");
+    return 0;
+}
