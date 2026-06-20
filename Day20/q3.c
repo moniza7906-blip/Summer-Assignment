961ad62fd57ced90b8afb88bb44bf696d3d48eaf
moniza7906-blip <moniza7906@gmail.com>
2026-06-20 20:04:30 +0530

    Day20 assignment

---
+++ Day20/q3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10], r, c;
+    int i, j, sum;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter elements of matrix:\n");
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++)
+    {
+        sum = 0;
+
+        for(j = 0; j < c; j++)
+        {
+            sum += a[i][j];
+        }
+
+        printf("Sum of row %d = %d\n", i + 1, sum);
+    }
+
+    return 0;
+}
---
+++ Day20/q4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10], r, c;
+    int i, j, sum;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter elements of matrix:\n");
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(j = 0; j < c; j++)
+    {
+        sum = 0;
+
+        for(i = 0; i < r; i++)
+        {
+            sum += a[i][j];
+        }
+
+        printf("Sum of column %d = %d\n", j + 1, sum);
+    }
+
+    return 0;
+}
